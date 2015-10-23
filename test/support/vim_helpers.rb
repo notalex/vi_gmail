005a27946771efa9474b6fc0dd2da8c1c3217256
Alex Johnson <notalexjohnson@gmail.com>
2015-10-24 02:42:58 +0530

    Setup vimrunner for vim testing

---
+++ test/support/vim_helpers.rb
@@ -0,0 +1,22 @@
+class MiniTest::Spec
+  private
+
+  def setup_vim
+    vim.command("cd #{ tmp_dir_path }")
+    plugin_path = File.expand_path('../../../lib/vim/', __FILE__)
+    vim.add_plugin(plugin_path, 'plugin/vi_gmail.vim')
+  end
+
+  def vim
+    @vim ||= Vimrunner.start_gvim
+  end
+
+  def kill_vim
+    FileUtils.remove_entry(tmp_dir_path)
+    vim.kill
+  end
+
+  def tmp_dir_path
+    @tmp_dir_path ||= Dir.mktmpdir('vimrunner')
+  end
+end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -9,6 +9,11 @@ require 'factory_girl'
 require 'forgery'
 require 'vcr'
 
+require 'vimrunner'
+require 'vimrunner/testing'
+require 'support/vim_helpers'
+require 'support/vimrunner_core_extensions'
+
 VCR.configure do |config|
   config.cassette_library_dir = 'fixtures/vcr_cassettes'
   config.hook_into :webmock
